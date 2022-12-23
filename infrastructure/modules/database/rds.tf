9f65cf096886f0bb593154946a6cefa4b7ecf82e
Obatula Fuad Adebari <74328942+Akinwalee@users.noreply.github.com>
2022-12-23 01:38:19 +0100

    Modified rds.tf

--- infrastructure/modules/database/rds.tf
+++ infrastructure/modules/database/rds.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_security_group" "rds_security_group" {
   name        = "rds_security_group"
   description = "Security group for RDS database"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = module.networking.aws_vpc.vpc.id
 
   ingress {
     from_port   = 5432
@@ -34,15 +34,22 @@ resource "aws_db_instance" "rds_database" {
   name                 = var.rds_db_name
   username             = var.rds_username
   password             = var.rds_password
-  vpc_id               = aws_vpc.vpc.id
-  security_group_ids   = [aws_security_group.rds_security_group.id]
-  subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  # vpc_id               = module.networking.aws_vpc.vpc.id
+  vpc_security_group_ids   = [aws_security_group.rds_security_group.id]
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
   publicly_accessible  = false
 }
 
 # Create an RDS subnet group
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds_subnet_group"
-  subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
+  subnet_ids = [module.networking.aws_subnet.public_subnet_1.id, module.networking.aws_subnet.public_subnet_2.id, module.networking.aws_subnet.public_subnet_3.id]
   description = "Subnet group for RDS database"
+}
+
+
+#import networking module
+module "networking" {
+  source = "../../modules/networking"
+  
 }
