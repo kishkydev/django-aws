e2d38566fac40ef5c2a83cfec68f5685787c86fd
Obatula Fuad Adebari <74328942+Akinwalee@users.noreply.github.com>
2022-12-23 18:34:02 +0100

    Modified networking/providers.tf

--- infrastructure/modules/networking/provider.tf
+++ infrastructure/modules/networking/provider.tf
@@ -1,20 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
-}
-
-provider "aws" {
-  alias   = "networking"
-  region  = var.region
-}
-
-terraform {
-  required_providers {
     namecheap = {
       source  = "namecheap/namecheap"
       version = ">= 2.0.0"
